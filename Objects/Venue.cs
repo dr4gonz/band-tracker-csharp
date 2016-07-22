8d194ef8c74c2b5fd217093b629abf149499b5b4
Matthew Reyes <mreyez@gmail.com>
2016-07-22 08:24:30 -0700

    add CRUD methods to venue.cs and test methods to venuetest.cs

--- Objects/Venue.cs
+++ Objects/Venue.cs
@@ -7,9 +7,215 @@ namespace BandTracker
 {
   public class Venue
   {
-    public Venue()
-    {
+    private int _id;
+    private string _name;
+    private string _phone;
+    private string _email;
 
+    public Venue(string name, string phone, string email, int id = 0)
+    {
+      _id = id;
+      _name = name;
+      _phone = phone;
+      _email = email;
+    }
+
+    public int GetId()
+      {
+        return _id;
+      }
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public string GetPhone()
+    {
+      return _phone;
+    }
+
+    public string GetEmail()
+    {
+      return _email;
+    }
+
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM venues;", conn);
+      cmd.ExecuteNonQuery();
+    }
+
+    public static List<Venue> GetAll()
+    {
+      List<Venue> allVenues = new List<Venue>{};
+
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM venues;", conn);
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int newVenueId = rdr.GetInt32(0);
+        string newVenueName = rdr.GetString(1);
+        string newVenuePhone = rdr.GetString(2);
+        string newVenueEmail = rdr.GetString(3);
+
+        Venue newVenue = new Venue(newVenueName, newVenuePhone, newVenueEmail, newVenueId);
+        allVenues.Add(newVenue);
+      }
+
+      if(rdr != null) rdr.Close();
+      if(conn != null) conn.Close();
+
+      return allVenues;
+    }
+
+    public override bool Equals(System.Object otherVenue)
+    {
+      if (!(otherVenue is Venue))
+      {
+          return false;
+      }
+      else
+      {
+        Venue newVenue = (Venue) otherVenue;
+        bool idEquality = (_id == newVenue.GetId());
+        bool nameEquality = (_name == newVenue.GetName());
+        bool phoneEquality = (_phone == newVenue.GetPhone());
+        bool emailEquality = (_email == newVenue.GetEmail());
+        return (idEquality && nameEquality && phoneEquality && emailEquality);
+      }
+    }
+
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO venues (name, phone, email) OUTPUT INSERTED.id VALUES(@VenueName, @VenuePhone, @VenueEmail);", conn);
+
+      SqlParameter venueNameParameter = new SqlParameter();
+      venueNameParameter.ParameterName = "@VenueName";
+      venueNameParameter.Value = this.GetName();
+      cmd.Parameters.Add(venueNameParameter);
+
+      SqlParameter venuePhoneParameter = new SqlParameter();
+      venuePhoneParameter.ParameterName = "@VenuePhone";
+      venuePhoneParameter.Value = this.GetPhone();
+      cmd.Parameters.Add(venuePhoneParameter);
+
+      SqlParameter venueEmailParameter = new SqlParameter();
+      venueEmailParameter.ParameterName = "@VenueEmail";
+      venueEmailParameter.Value = this.GetEmail();
+      cmd.Parameters.Add(venueEmailParameter);
+
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+
+      if(rdr != null) rdr.Close();
+      if(conn != null) conn.Close();
+    }
+
+    public static Venue Find(int id)
+    {
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM venues WHERE id = @VenueId;", conn);
+      SqlParameter venueIdParameter = new SqlParameter();
+      venueIdParameter.ParameterName = "@VenueId";
+      venueIdParameter.Value = id.ToString();
+      cmd.Parameters.Add(venueIdParameter);
+
+      rdr = cmd.ExecuteReader();
+
+      int foundVenueId = 0;
+      string foundVenueName = null;
+      string foundVenuePhone = null;
+      string foundVenueEmail = null;
+
+      while(rdr.Read())
+      {
+          foundVenueId = rdr.GetInt32(0);
+          foundVenueName = rdr.GetString(1);
+          foundVenuePhone = rdr.GetString(2);
+          foundVenueEmail = rdr.GetString(3);
+      }
+      Venue newVenue = new Venue(foundVenueName, foundVenuePhone, foundVenueEmail, foundVenueId);
+
+      if(rdr != null) rdr.Close();
+      if(conn != null) conn.Close();
+
+      return newVenue;
+    }
+
+    public void Update(string newName, string newPhone, string newEmail)
+    {
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("UPDATE venues SET name = @NewName, phone = @NewPhone, email = @NewEmail OUTPUT INSERTED.name, INSERTED.phone, INSERTED.email WHERE id = @VenueId;", conn);
+
+      SqlParameter newNameParameter = new SqlParameter();
+      newNameParameter.ParameterName = "@NewName";
+      newNameParameter.Value = newName;
+      cmd.Parameters.Add(newNameParameter);
+
+      SqlParameter newPhoneParameter = new SqlParameter();
+      newPhoneParameter.ParameterName = "@NewPhone";
+      newPhoneParameter.Value = newPhone;
+      cmd.Parameters.Add(newPhoneParameter);
+
+      SqlParameter newEmailParameter = new SqlParameter();
+      newEmailParameter.ParameterName = "@NewEmail";
+      newEmailParameter.Value = newEmail;
+      cmd.Parameters.Add(newEmailParameter);
+
+      SqlParameter venuetIdParameter = new SqlParameter();
+      venuetIdParameter.ParameterName = "@VenueId";
+      venuetIdParameter.Value = this.GetId();
+      cmd.Parameters.Add(venuetIdParameter);
+
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._name = rdr.GetString(0);
+        this._phone = rdr.GetString(1);
+        this._email = rdr.GetString(2);
+      }
+
+      if(rdr != null) rdr.Close();
+      if(conn != null) conn.Close();
+    }
+
+    public void Delete()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("DELETE FROM venues WHERE id = @VenueId;", conn);
+
+      SqlParameter venueIdParameter = new SqlParameter();
+      venueIdParameter.ParameterName = "@VenueId";
+      venueIdParameter.Value = this.GetId();
+      cmd.Parameters.Add(venueIdParameter);
+
+      cmd.ExecuteNonQuery();
+
+      if(conn != null) conn.Close();
     }
   }
 }
--- Tests/VenueTest.cs
+++ Tests/VenueTest.cs
@@ -13,5 +13,112 @@ namespace BandTracker
       // DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=band_tracker_test;Integrated Security=SSPI;";
       DBConfiguration.ConnectionString = "Data Source=DESKTOP-7OLC9FT\\SQLEXPRESS;Initial Catalog=band_tracker_test;Integrated Security=SSPI;";
     }
+
+    [Fact]
+    public void Venue_DatabaseEmptyAtFirst()
+    {
+      //Arrange, Act
+      int result = Venue.GetAll().Count;
+      //Assert
+      Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Venue_ChecksIfVenuesAreEqual_returnsTrue()
+    {
+      //Arrange, Act
+      Venue firstVenue = new Venue("Mississippi Studios", "503.555.5555", "email@email.com");
+      Venue secondVenue = new Venue("Mississippi Studios", "503.555.5555", "email@email.com");
+      //Assert
+      Assert.Equal(firstVenue, secondVenue);
+    }
+
+    [Fact]
+    public void Venues_SavesToDatabase()
+    {
+      //Arrange
+      Venue newVenue = new Venue("Mississippi Studios", "503.555.5555", "email@email.com");
+      newVenue.Save();
+      //Act
+      List<Venue> result = Venue.GetAll();
+      List<Venue> testList = new List<Venue>{newVenue};
+      //Assert
+      Assert.Equal(testList, result);
+    }
+
+    [Fact]
+    public void Venue_SavesSavesWithID()
+    {
+      //Arrange
+      Venue newVenue = new Venue("Mississippi Studios", "503.555.5555", "email@email.com");
+      newVenue.Save();
+      //Act
+      Venue savedVenue = Venue.GetAll()[0];
+      int result = newVenue.GetId();
+      int testId = savedVenue.GetId();
+      //Assert
+      Assert.Equal(testId, result);
+    }
+
+    [Fact]
+    public void Venue_FindsVenueInDatabase()
+    {
+      //Arrange
+      Venue newVenue = new Venue("Mississippi Studios", "503.555.5555", "email@email.com");
+      newVenue.Save();
+      //Act
+      Venue foundVenue = Venue.Find(newVenue.GetId());
+      //Assert
+      Assert.Equal(newVenue, foundVenue);
+    }
+
+    [Fact]
+    public void Venue_Update_UpdatesVenueInDatabase()
+    {
+      //Arrange
+      Venue newVenue = new Venue("Mississippi Studios", "503.555.5555", "email@email.com");
+      newVenue.Save();
+      string newName = "Revolution Hall";
+      string newPhone = "503.666.6666";
+      string newEmail = "none@none.com";
+      //Act
+      newVenue.Update(newName, newPhone, newEmail);
+
+      string updateName = newVenue.GetName();
+      string updatePhone = newVenue.GetPhone();
+      string updateEmail = newVenue.GetEmail();
+      //Assert
+      Assert.Equal(newName, updateName);
+      Assert.Equal(newPhone, updatePhone);
+      Assert.Equal(newEmail, updateEmail);
+    }
+
+    [Fact]
+    public void Venues_Delete_DeletesVenueFromDatabase()
+    {
+      //Arrange
+      string name1 = "Mississippi Studios";
+      string phone1 = "503.555.5555";
+      string email1 = "email@email.com";
+      Venue testVenue1 = new Venue(name1, phone1, email1);
+      testVenue1.Save();
+
+      string name2 = "Revolution Hall";
+      string phone2 = "503.666.6666";
+      string email2 = "none@none.com";
+      Venue testVenue2 = new Venue(name2, phone2, email2);
+      testVenue2.Save();
+      //Act
+      testVenue2.Delete();
+      List<Venue> resultVenue = Venue.GetAll();
+      List<Venue> testVenue = new List<Venue> {testVenue1};
+      //Assert
+      Assert.Equal(testVenue, resultVenue);
+    }
+
+    public void Dispose()
+      {
+        Venue.DeleteAll();
+      }
   }
 }
