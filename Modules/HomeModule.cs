4ac5fe6021f0473513f8fa5bbdd9928f0b678fd3
Matthew Reyes <mreyez@gmail.com>
2016-07-22 09:48:37 -0700

    begin adding views to project

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,6 +9,22 @@ namespace BandTracker
     public HomeModule()
     {
       Get["/"] = _ => View["index.cshtml"];
+      Get["/bands"] = _ => {
+        List<Band> allBands = Band.GetAll();
+        return View["bands.cshtml", allBands];
+      };
+      Get["/bands/{id}"] = parameters => {
+        Band newBand = Band.Find(parameters.id);
+        return View["band.cshtml", newBand];
+      };
+      Get["/venues"] = _ => {
+        List<Venue> allVenues = Venue.GetAll();
+        return View["venues.cshtml", allVenues];
+      };
+      Get["/venues/{id}"] = parameters => {
+        Venue newVenue = Venue.Find(parameters.id);
+        return View["venue.cshtml", newVenue];
+      };
     }
   }
 }
